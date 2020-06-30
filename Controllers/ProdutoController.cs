f2b1800997799a78437748683d28780739dcce5e
Vinicius Moreira Souto <vsouto@apoioprodesp.sp.gov.br>
2020-06-30 12:01:42 -0300

    Add project files.

---
+++ Controllers/ProdutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace mvc1.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public string Index()
+        {
+            var https = HttpContext.Request.IsHttps;
+            var caminho = HttpContext.Request.Path;
+            var status = HttpContext.Response.StatusCode;
+            var conexao = HttpContext.Connection.ToString();
+
+            return "Index da Produto\r\n" + https + "\r\n" + caminho + "\r\n" + status + "\r\n" + conexao;
+        }
+
+        public IActionResult Detalhe()
+        {
+            //return RedirectToAction("Index", "Home",new { pagina = 1, ordem = "nome" });
+            //var pessoa = new { ID = 1, Nome = "Vinicius" };
+            //return new ObjectResult(pessoa);
+
+            return View();                                                                                                                                                  
+        }
+    }
+}
